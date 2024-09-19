325f50a607ad479f619eac3ab5864556f2224a74
Furuta Naoki <fn.nk.2002@gmail.com>
2024-09-19 11:59:47 +0900

    ガチャのAPIのエンドポイントを作成

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, Response
 from gacha import gacha
+import json
 
 app = Flask(__name__)
 
 @app.route('/gacha', methods=['GET'])
-def gacha():
-    return render_template('index.html')
+def gacha_api():
+    result_json = json.dumps(gacha())
+    return Response(result_json, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
